c9c51a5ef5587d20f6a1c9613c56bc1465f36b6f
Bladimir Minga <bsminga@pichincha.com>
2022-07-28 16:01:55 -0500

    first commit

---
+++ room-service/src/main/java/com/frankmoley/lil/lq/ReadinessProbe.java
@@ -0,0 +1,24 @@
+package com.frankmoley.lil.lq;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+@Readiness
+@ApplicationScoped
+public class ReadinessProbe implements HealthCheck {
+
+    @Inject
+    RoomService roomService;
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("Custom Readiness Check")
+                .up()
+                .withData("roomCount", roomService.getAllRooms().size())
+                .build();
+    }
+}
